a486770a737da131f2e1d1fcdd6680b189651b8f
khalilhamdii <khaalil.hamdi@gmail.com>
2020-10-20 09:02:41 +0100

    Add model user and validations

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :username, presence: true, length: { minimum: 2 }, uniqueness: true
+  validates :email, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+end
